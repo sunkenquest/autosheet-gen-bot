ba5f2fe0e14ff464afd5d36d2386fb8288900f6a
sunkenquest <sunkenquest@gmail.com>
2024-09-27 14:03:33 +0800

    add delete workflow

--- autodel.sh
+++ autodel.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-if [[ -f data.txt ]]; then
+# Empty data.txt
+if [[ -f ./data.txt ]]; then
     > data.txt
+    echo "data.txt emptied."
 else
     echo "data.txt not found!"
     exit 1
 fi
 
-
-if [[ -f result.txt ]]; then
+# Empty result.txt
+if [[ -f ./result.txt ]]; then
     > result.txt
+    echo "result.txt emptied."
 else
     echo "result.txt not found!"
     exit 1
 fi
 
 echo "empty na :>"
+
+# Notify via Weekly Webhook
+RESPONSE=$(curl -s -X POST "$WEEKLY_WEBHOOK_URL" \
+    -H "Content-Type: application/json" \
+    -d "{\"text\":\"The file data.txt has been successfully emptied.\"}")
+
+echo "Notification sent to bot (Weekly): $RESPONSE"
+
+# Notify via Daily Webhook
+RESPONSE=$(curl -s -X POST "$DAILY_WEBHOOK_URL" \
+    -H "Content-Type: application/json" \
+    -d "{\"text\":\"The file result.txt has been successfully emptied.\"}")
+
+echo "Notification sent to bot (Daily): $RESPONSE"
