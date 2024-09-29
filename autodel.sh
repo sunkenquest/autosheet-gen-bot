9865448bfa9ccd913eabab09893e07106f51488f
sunkenquest <sunkenquest@gmail.com>
2024-09-29 09:02:47 +0800

    fix desc

--- autodel.sh
+++ autodel.sh
@@ -14,6 +14,6 @@ echo "empty na :>"
 # Notify via Daily Webhook
 RESPONSE=$(curl -s -X POST "$DAILY_WEBHOOK_URL" \
     -H "Content-Type: application/json" \
-    -d "{\"text\":\"pool emptied :>.\"}")
+    -d "{\"text\":\"The data file has been successfully cleared.\"}")
 
 echo "Notification sent to bot (Daily): $RESPONSE"
